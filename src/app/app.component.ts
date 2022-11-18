9c9e8c90b5719cebfd9dfa261a11580d2303e893
khoanguyn1411 <khoaah1411@gmail.com>
2022-11-18 23:55:44 +0700

    Add eslint rxjs configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'rhythm';
+  public asd$: Observable<number> = of(123)
 }
